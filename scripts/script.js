8e83514b377d5a12ec4b4630cc26e21b8ea63bb4
prince adegah <adegah.prince@yahoo.com>
2021-03-25 07:49:15 +0000

    implemented details page

--- scripts/script.js
+++ scripts/script.js
@@ -5,14 +5,13 @@ window.localStorage.setItem('pdt1-price','120');
 window.localStorage.setItem('pdt1-path1','assets/images/img1.PNG');
 window.localStorage.setItem('pdt1-path2','assets/images/img1.PNG');
 window.localStorage.setItem('pdt1-path3','assets/images/img1.PNG');
-window.localStorage.setItem('pdt1-path4','assets/images/img1.PNG');
 window.localStorage.setItem('pdt1-red','assets/images/img1.PNG');
 window.localStorage.setItem('pdt1-blue','assets/images/img1.PNG');
 window.localStorage.setItem('pdt1-black','assets/images/img1.PNG');
 
 /*product 1 reviews*/
 window.localStorage.setItem('pdt1-rev1','sweater');
-window.localStorage.setItem('pdt1-rev2','sweater');
+window.localStorage.setItem('pdt1-rev2','sweater');z
 window.localStorage.setItem('pdt1-rev3','sweater');
 window.localStorage.setItem('pdt1-rev4','sweater');
 
@@ -59,4 +58,33 @@ window.localStorage.setItem('pdt4-rev3','sweater');
 window.localStorage.setItem('pdt4-rev4','sweater');
 
 
+
 document.getElementById('pdt').src = window.localStorage.getItem('pdt1-path');
+document.getElementById('description').innerHTML = window.localStorage.getItem('pdt1-description');
+document.getElementById('price').innerHTML = window.localStorage.getItem('pdt1-price');
+
+
+/*
+Functions for counter
+*/
+
+var click = Number(document.getElementById('item-nbr').innerText)
+var minimum = 1;
+
+function decSize()
+{
+    click -= 1;
+    if(click < 1)
+    {
+        click = minimum;
+        return document.getElementById('item-nbr').innerText = minmum;
+    }
+    
+    document.getElementById('item-nbr').innerText = click;
+}
+
+function incSize()
+{
+    click += 1;
+    document.getElementById('item-nbr').innerText = click;
+}
